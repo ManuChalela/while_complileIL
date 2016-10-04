6022e743aa556e687a7cea24922a789e0ad58b5f
santitamba <santiago.tambasco@gmail.com>
2016-10-03 22:27:56 -0300

    primer commit

---
+++ src/main/java/examples/compil2015ut4/ast/Sequence.java
@@ -0,0 +1,67 @@
+package examples.compil2015ut4.ast;
+
+import java.util.*;
+import examples.compil2015ut4.behaviour.*;
+import java.io.*;
+
+/** Representación de las secuencias de sentencias.
+*/
+public class Sequence extends Stmt {
+	public final Stmt[] statements;
+
+	public Sequence(Stmt[] statements) {
+		this.statements = statements;
+	}
+
+	@Override public String unparse() {
+		String result = "{ ";
+					for (Stmt statement : statements) result += statement.unparse() +" ";
+					return result +"}";
+	}
+
+	@Override public State evaluate(State state) {
+		for (Stmt statement : statements) state = statement.evaluate(state);
+					return state;
+	}
+
+	@Override public Set<String> freeVariables(Set<String> vars) {
+		for (Stmt statement : statements) vars = statement.freeVariables(vars);
+					return vars;
+	}
+
+	@Override public int maxStackIL() {
+		int result = 0;
+					for (Stmt statement : statements) result = Math.max(result, statement.maxStackIL());
+					return result;
+	}
+
+	@Override public CompilationContextIL compileIL(CompilationContextIL ctx) {
+		throw new Error("Method compileIL not implemented!");
+	}
+
+	@Override public String toString() {
+		return "Sequence("+ Arrays.toString(statements) +")";
+	}
+
+	@Override public int hashCode() {
+		int result = 1;
+		result = result * 31 + Arrays.hashCode(this.statements);
+		return result;
+	}
+
+	@Override public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null || getClass() != obj.getClass()) return false;
+		Sequence other = (Sequence)obj;
+		return Arrays.equals(this.statements, other.statements);
+	}
+
+	public static Sequence generate(Random random, int min, int max) {
+		Stmt[] statements; 
+		statements = new Stmt[random.nextInt(Math.max(0, max)+1)];
+		for (int i = 0; i < statements.length; i++) {
+			statements[i] = Stmt.generate(random, min-1, max-1);
+		}
+		return new Sequence(statements);
+	}
+}
---
+++ src/main/java/examples/compil2015ut4/ast/Variable.java
@@ -0,0 +1,58 @@
+package examples.compil2015ut4.ast;
+
+import java.util.*;
+import examples.compil2015ut4.behaviour.*;
+import java.io.*;
+
+/** Representación de usos de variable en expresiones.
+*/
+public class Variable extends AExp {
+	public final String id;
+
+	public Variable(String id) {
+		this.id = id;
+	}
+
+	@Override public String unparse() {
+		return id;
+	}
+
+	@Override public Double evaluate(State state) {
+		return state.get(id);
+	}
+
+	@Override public Set<String> freeVariables(Set<String> vars) {
+		vars.add(id); return vars;
+	}
+
+	@Override public int maxStackIL() {
+		return 1;
+	}
+
+	@Override public CompilationContextIL compileIL(CompilationContextIL ctx) {
+		throw new Error("Method compileIL not implemented!");
+	}
+
+	@Override public String toString() {
+		return "Variable("+ id +")";
+	}
+
+	@Override public int hashCode() {
+		int result = 1;
+		result = result * 31 + (this.id == null ? 0 : this.id.hashCode());
+		return result;
+	}
+
+	@Override public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null || getClass() != obj.getClass()) return false;
+		Variable other = (Variable)obj;
+		return (this.id == null ? other.id == null : this.id.equals(other.id));
+	}
+
+	public static Variable generate(Random random, int min, int max) {
+		String id; 
+		id = ""+"abcdefghijklmnopqrstuvwxyz".charAt(random.nextInt(26));
+		return new Variable(id);
+	}
+}
